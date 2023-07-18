2460720dd08f85269767c80048721a6d3425b08b
Glitch (weathertop-anna-ballot) <none>
2023-07-18 16:32:30 +0000

    🎙️◀️ Updated with Glitch

---
+++ controllers/dashboard-controller.js
@@ -0,0 +1,27 @@
+import { stationStore } from "../models/station-store.js";
+import { accountsController } from "./accounts-controller.js";
+
+export const dashboardController = {
+  async index(request, response) {
+    const loggedInUser = await accountsController.getLoggedInUser(request);
+    const viewData = {
+      title: "Station Dashboard",
+      stations: await stationStore.getStationsByUserId(loggedInUser._id),
+    };
+    console.log("dashboard rendering");
+    response.render("dashboard-view", viewData);
+  },
+
+  async addStation(request, response) {
+    const loggedInUser = await accountsController.getLoggedInUser(request);
+    const newStation = {
+      title: request.body.title,
+      latitude: Number(request.body.latitude),
+      longitude: Number(request.body.longitude),
+      userid: loggedInUser._id,
+    };
+    console.log(`adding station ${newStation.title}`);
+    await stationStore.addStation(newStation);
+    response.redirect("/dashboard");
+  },
+};
